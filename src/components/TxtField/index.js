2c3a9448ae20abf7b9a87a50c2d463baa4a98614
Nexusf1re <matteussantos30@hotmail.com>
2024-09-02 17:43:46 -0300

    First commit for 'Organograma'

---
+++ src/components/TxtField/index.js
@@ -0,0 +1,13 @@
+import "./TxtField.css"
+
+const TxtField = () => {
+
+    return(
+        <div className="campo-texto">   
+            <label> Nome </label>
+            <input placeholder="Digite o seu nome" />
+        </div>
+    )
+}
+
+export default TxtField
