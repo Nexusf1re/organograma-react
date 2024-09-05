de1aeeb4d24536b8ea9676b4651859321d5a1bf5
Nexusf1re <matteussantos30@hotmail.com>
2024-09-05 10:20:41 -0300

    Default commit

--- src/components/TxtField/index.js
+++ src/components/TxtField/index.js
@@ -2,11 +2,18 @@ import "./TxtField.css"
 
 const TxtField = (props) => {
 
+    let valor = 'Matteus Abreu'
+
+    const aoDigitar = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
+    }
+
     return(
         <div className="campo-texto">   
             <label> {props.label} </label>
 
-            <input required={props.obrigatorio} placeholder={props.placeholder} />
+            <input value={valor} onChange={aoDigitar} required={props.obrigatorio} placeholder={props.placeholder} />
         </div>
     )
 }
